7743bc73f6f717e4013b2c398225273fc92435d6
chinmay7488 <chinmayjain8314@gmail.com>
2024-08-25 13:12:09 +0530

    updating

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
 python3.9 -m ensurepip --user
 python3.9 -m pip install -r requirement.txt
-python3.9  manage.py collectstatic --noinput
+# python3.9  manage.py collectstatic --noinput
 echo "BUILD END"
